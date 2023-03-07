0ef9374aa6c813fd283f6dd9ad3fff7aa3865a5f
raster-Q <119475152+raster-Q@users.noreply.github.com>
2023-03-08 05:23:35 +0900

    だめだ2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-//(function () { //即時関数発動
+(function () { //即時関数発動
 
 ////////ここから/////////////////
 
@@ -22,14 +22,14 @@ function shuffle(arr) {
 //------変数定義-------------
 //---DOM操作関連
 const cardImg = document.getElementById("cardImg"); //カード画像表示
-const cardPosition = document.getElementById("cardPosition"); //正逆表示
+//const cardPosition = document.getElementById("cardPosition"); //正逆表示
 const cardName = document.getElementById("cardName"); //カード名表示
 
 //---ボタン関連
-const selectPosition = document.getElementById("selectPosition"); //正逆ラジオボタン
-const shuffleCard = document.getElementById("shuffleCard"); //シャッフルボタン
-let dealCard = document.getElementById("dealCard"); //1枚引くボタン
-const resetAll = document.getElementById("resetAll"); //リセットボタン
+//const selectPosition = document.getElementById("selectPosition"); //正逆ラジオボタン
+const shuffleButton = document.querySelector("#shuffleButton"); //シャッフルボタン
+const dealButton = document.querySelector("#dealButton"); //1枚引くボタン
+//const resetButton = document.getElementById("resetButton"); //リセットボタン
 
 //---カードデータ関連
 const cac = [
@@ -57,31 +57,33 @@ const cac = [
 //---正逆関連
 
 //---各種変数
-let arrNums = []; //シャッフル完了数字格納
-let result = null; //1枚引き用
+var arrNums = []; //シャッフル完了数字格納
+var result = 0; //1枚引き用
 
 //------変数、ここまで--------
 
 ////////初期配置////////////////////////////////////////
 arrNums = [...Array(cac.length).keys()];
-console.log(arrNums);
+console.log('111' + cardImg);
 ////////初期配置、ここまで////////////////////////////////////////
 
 //------スクリプト記述--------
 //---シャッフルボタン、イベント
-// shuffleCard.addEventListener('click',() => {
-//    shuffle(arrNums);
-//  });
+  shuffleButton.addEventListener('click', function () {
+         shuffle(arrNums);
+         console.log(arrNums);
+    });
 
 //---1枚引くボタン、イベント
-// dealCard.addEventListener('click', function ()  {
-//    result = arrNums.pop();
-console.log("3");
-//    cardImg.innerHTML = `<img src = "https://upload.wikimedia.org/wikipedia/commons/d/d7/RWS_Tarot_13_Death.jpg" id="dealCard" />`;
-
-//  });
+  dealButton.addEventListener("click",function () {
+    result = arrNums.pop();
+    console.log(result);
+    console.log(cac[result].img);
+    cardImg.innerHTML = `<img src="${cac[result].img}" id ="card"></img>`;
+    cardName.innerHTML = `${cac[result].name}`
+  });
 //------スクリプト、ここまで---
 
 //////ここまで////////////////////
 
-//}());          //即時関数終了
+}())          //即時関数終了
